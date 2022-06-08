322133192880e60d4948455501afa88b162ba91e
Gnod <gnodxm@gmail.com>
2022-06-08 17:19:35 +0700

    Adding UI to game

--- script.js
+++ script.js
@@ -1,49 +1,68 @@
 const choice = ['Rock','Paper', 'Scissors'];
-let playerWin=0;
-let computerWin=0;
+let win=0;
+let lose=0;
+let draw = 0;
+let humanPlay;
+
+const winScore = document.querySelector('.winScore');
+const drawScore = document.querySelector('.drawScore');
+const loseScore = document.querySelector('.loseScore');
+
+const btns = Array.from(document.querySelectorAll('.btn'));
+
+
 function computerPlay () {
     return choice[Math.floor(Math.random()*choice.length)]
-}
-function humanPlay () {
-
-    let weapon = prompt("Select your weapon","Rock").toLowerCase();
-    return weapon.charAt(0).toUpperCase() + weapon.slice(1);
 }
 
 function playRound(playerSelection,computerSelection){
     let result;
-    playerSelection = humanPlay();
+    playerSelection = humanPlay;
     computerSelection = computerPlay();
     if (playerSelection == computerSelection) {
         result = "Draw";
+        draw++;
+        drawScore.textContent = "Draw: " + draw;
     } else {
         switch (playerSelection+computerSelection){
             case "RockScissors":
             case "ScissorsPaper":
             case "PaperRock":
-                result = 'You win';
-                playerWin++;
+                result = `You win. ${playerSelection} beats ${computerSelection}`;
+                win++;
+                winScore.textContent="Win: " + win;
                 break
             default:
-                result = 'You lose';
-                computerWin++;
+                result = `You lose. ${playerSelection} is beaten by ${computerSelection}`;
+                lose++;
+                loseScore.textContent="Lose: " +lose;
         }
     }
-    console.log(result);
-
-    return result;
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    body.appendChild(div);
+    div.textContent=result;
 
 }
-
-function game(){
-    
-    for (let i = 0; i < 5; i++) {
+btns.forEach(btn=>{
+    btn.addEventListener('click',e => {
+        humanPlay=btn.getAttribute('id');
         playRound();
-
+        winCheck();
+    })
+});
+function winCheck(){
+    if (win==5) {
+        window.alert('YOU ARE WINNER');
+        reset()
+    } else if (lose==5) {
+        window.alert('YOU ARE LOSERRRRRRRR');
+        reset()
     };
-    let finalResult = playerWin>computerWin?"Winner":
-    playerWin == computerWin?"Drawer":"Loser";
-    return finalResult;
 }
 
-console.log(game());
+function reset(){
+    win = 0;
+    lose =0;
+    draw =0;
+}
