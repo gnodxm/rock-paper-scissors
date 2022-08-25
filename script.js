6c5fdc9c9cf5a497779033037a295a2a0ada6bec
Gnod <gnodxm@gmail.com>
2022-08-25 16:24:13 +0700

    update UI

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function updateScore () {
+	const playerScoreDiv = document.querySelector('.humanScore')
+	const computerScoreDiv = document.querySelector('.computerScore')
+
+	playerScoreDiv.textContent=playerScore;
+	computerScoreDiv.textContent=computerScore;
+
+	if (game()) {
+		displayModal(game())
+	}
+}
+
+const weapons = document.querySelectorAll('.left .weapon')
+weapons.forEach(weapon => {
+	weapon.addEventListener('click',e => {
+		const playerSelection = e.target.dataset.id;
+		const computerSelection = getComputerChoice();
+		displayChoice(playerSelection, computerSelection)
+		playRound(playerSelection, computerSelection)
+		updateScore()
+	})
+})
+
+
 function getComputerChoice () {
 	const options = ['Rock', 'Paper', 'Scissors']
 	const randomIndex = Math.floor(Math.random()*options.length)
 	return options[randomIndex]
 }
 
-function getPlayerChoice () {
-	const choice = prompt('Enter your choice: Rock? Paper? Scissors?')
-	return choice[0].toUpperCase().concat(choice.slice(1).toLowerCase())
+function displayChoice (playerSelection, computerSelection) {
+	const computerWeapon = document.querySelector(`.right .weapon[data-id='${computerSelection}']`);
+	const playerWeapon = document.querySelector(`.left .weapon[data-id='${playerSelection}']`);
+
+	computerWeapon.classList.add('computerSelection')
+	playerWeapon.classList.add('playerSelection')
+
+	computerWeapon.addEventListener('transitionend', ()=> {
+		computerWeapon.classList.remove('computerSelection')
+	})
+	playerWeapon.addEventListener('transitionend', ()=> {
+		playerWeapon.classList.remove('playerSelection')
+	})
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -18,29 +55,36 @@ function playRound(playerSelection, computerSelection) {
 		playerSelection === 'Paper' && computerSelection === 'Scissors' ||
 		playerSelection === 'Scissors' && computerSelection === 'Paper'
 	) {
-		result = `You lose! ${computerSelection} beats ${playerSelection}`
-	} else result = `You win! ${playerSelection} beats ${computerSelection}`
-	console.log(result);
+		computerScore++;
+/* 		result = `You lose! ${computerSelection} beats ${playerSelection}` */
+	} else {
+		playerScore++
+/* 		result = `You win! ${playerSelection} beats ${computerSelection}` */
+	}
 	return result;
 }
 
 
 
 function game() {
-	let playerScore = 0;
-	let computerScore = 0;
-	for (let i =0; i< 5; i++) {
-		const result = playRound(getPlayerChoice(),getComputerChoice());
-		if (result.includes('win')) {
-			playerScore++
-		} else if (result.includes('lose')) {
-			computerScore++
-		}
-	}
-	if (playerScore > computerScore) {
-		return 'Win'
-	} else if (playerScore < computerScore) {
-		return 'Lose'
-	}
-	return 'Tie'
+	
+	if (playerScore == 5) {
+
+		return 'You win!'
+	} else if (computerScore == 5) {
+		return 'Game over!'
+	} 
+	return;
 }
+
+function displayModal (winner) {
+	const modal = document.querySelector('.modal')
+	const gameResult = document.querySelector('.gameResult')
+	modal.setAttribute('style','visibility:visible')
+	gameResult.textContent = winner
+}
+
+const reloadBtn = document.querySelector('.reload')
+reloadBtn.addEventListener('click', () => {
+	window.location.reload()
+})
